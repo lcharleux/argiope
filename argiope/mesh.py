77361b4158de85a30d2b28c8493e5b4aeeceab41
lcharleux <ludovic.charleux@univ-smb.fr>
2016-09-26 17:10:55 +0200

    Improved XDMF export

--- argiope/mesh.py
+++ argiope/mesh.py
@@ -210,13 +210,21 @@ class Mesh(object):
   def __repr__(self): return "*Nodes:\n{0}\n*Elements\n{1}".format(
                           str(self.nodes), str(self.elements))
   
-  def __init__(self, nlabels, coords, elabels, etypes, connectivity, nsets = {}, esets = {}, surfaces = {}, h5path = None):
+  def __init__(self, nlabels, coords, elabels, etypes, connectivity, nsets = {}, esets = {}, surfaces = {}, h5path = None, master = None):
     self.nodes    = Nodes(    labels = nlabels, coords = coords, sets = nsets, 
                               master = self)
     self.elements = Elements( labels = elabels, connectivity = connectivity, 
                               etypes = etypes, sets = esets, 
                               surfaces = surfaces, master = self)
     self.h5path = h5path
+
+
+    
+class Model(object):
+  """
+  A class to rule them all...
+  """
+  pass    
 ################################################################################
     
 
@@ -272,6 +280,98 @@ def read_msh(path):
               connectivity = elements["conn"],
               etypes = elements["etype"],
               esets = elements["sets"])
+
+def read_inp(path):
+  """
+  Reads Abaqus inp file
+  """
+  def lineInfo(line):
+    out =  {"type": "data"}
+    if line[0] == "*":
+      if line[1] == "*": 
+        out["type"] = "comment"
+        out["text"] = line[2:]
+      else:
+        out["type"] = "command"
+        words = line[1:].split(",")
+        out["value"] = words[0].strip()
+        out["options"] = {}
+        for word in words[1:]:
+          key, value =  [s.strip() for s in word.split("=")]
+          out["options"][key] = value
+    return out
+  def elementMapper(inpeltype):
+    if inpeltype == "t3d2": return "Line2"
+    if inpeltype[:3] in ["cps", "cpe", "cax"]:
+      if inpeltype[3] == "3": return "Tri3"
+      if inpeltype[3] == "4": return "Quad4"
+    if inpeltype[:3] in ["c3d"]:
+      if inpeltype[3] == "4": return "Tetra4"
+      if inpeltype[3] == "5": return "Pyra5"
+      if inpeltype[3] == "6": return "Prism6"
+      if inpeltype[3] == "8": return "Hexa8"
+    
+      
+  # Output mesh
+  m = Mesh()
+  # File preprocessing
+  lines = np.array([l.strip().lower() for l in open(path).readlines()])
+  # Data processing
+  env = None
+  setlabel = None
+  for line in lines: 
+    d = lineInfo(line)
+    if d["type"] == "command": 
+      env = d["value"]
+      # Nodes
+      if env == "node":
+        opt = d["options"]
+        currentset = None
+        if "nset" in opt.keys(): currentset = opt["nset"]
+          
+      # Elements
+      if env == "element":
+        opt = d["options"]
+        eltype = elementMapper(opt["type"])
+        currentset = None
+        if "elset" in opt.keys(): currentset = opt["elset"]
+          
+      # Nsets
+      if env == "nset":
+        opt = d["options"]      
+        currentset = opt["nset"]
+        
+      # Elsets     
+      if env == "elset":
+        opt = d["options"]      
+        currentset = opt["elset"]
+             
+    if d["type"] == "data": 
+      words = line.strip().split(",")
+      if env == "node": 
+        label  = int(words[0])
+        coords = np.array( [float(w) for w in words[1:]], dtype = np.float64 )
+        if currentset == None: 
+          m.nodes[label] = Node(coords = coords)
+        else:
+          m.nodes[label] = Node(coords = coords, sets = set([currentset]))  
+            
+      if env == "element": 
+        label  = int(words[0])
+        conn = np.array( [int(w) for w in words[1:]], dtype = np.int32)
+        if currentset == None: 
+          m.elements[label] = globals()[eltype](conn = conn)
+        else:
+          m.elements[label] = globals()[eltype](conn = conn, sets = set([currentset]))
+      
+      if env == "nset": 
+        [m.nodes[int(w)].sets.add(currentset) for w in words if len(w) != 0]   
+        
+      if env == "elset": 
+        [m.elements[int(w)].sets.add(currentset) for w in words if len(w) != 0]         
+        
+              
+  return m
   
 ################################################################################
 # WRITERS
@@ -341,6 +441,112 @@ def write_xdmf(mesh, path, dataformat = "XML"):
 # TESTS
 ################################################################################
 if __name__ == '__main__':
-    m2 = read_msh("../doc/mesh/demo.msh")
-    write_xdmf(m2, "test", dataformat = "HDF")
-
+  import time
+  print "# READING MESH"
+  t0 = time.time()
+  m2 = read_msh("../doc/mesh/demo.msh")
+  t1 =  time.time()
+  print "# => {0:.2f}s".format(t1-t0)
+  
+  print "# EXPORTING MESH"
+  """
+  write_xdmf(m2, "test", dataformat = "XML")
+  
+  """
+  
+  
+  # TESTS
+  mesh = m2
+  dataformat = "HDF"
+  path = "test"
+  
+  #XDMF PATTERN
+  pattern = """<?xml version="1.0"?>
+<!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
+<Xdmf Version="2.0" xmlns:xi="http://www.w3.org/2001/XInclude">
+   <Domain>
+     <Grid Name="Mesh">
+       <Topology TopologyType="Mixed" NumberOfElements="#ELEMENT_NUMBER">
+         <DataItem Format="#DATAFORMAT" Dimensions="#CONN_DIMENSION">
+#CONN_PATH
+         </DataItem>
+       </Topology>
+       <Geometry GeometryType="XYZ">
+         <DataItem Format="#DATAFORMAT" Dimensions="#NODE_NUMBER 3">
+#NODE_PATH
+         </DataItem>
+       </Geometry>  
+     </Grid>
+   </Domain>
+</Xdmf>"""
+  
+  # MAPPINGS
+  cell_map = {
+      "Tri3":   4,
+      "Quad4":  5,
+      "Tetra4": 6,
+      "Pyra5":  7,
+      "Prism6": 8,
+      "Hexa8":  9}
+  # REFERENCES
+  nodes, elements = mesh.nodes.data, mesh.elements.data
+  # NUMBERS
+  Ne, Nn = len(elements), len(nodes)
+  # NODES
+  nodes_map = np.arange(nodes.index.max()+1)
+  nodes_map[nodes.index] = np.arange(len(nodes.index))
+  nodes_map[0] = -1
+  # ELEMENTS
+  cols = ["n{0}".format(i) for i in xrange(elements.shape[1]-1)]
+  connectivities  = mesh.elements.data[cols].as_matrix()
+  connectivities[np.isnan(connectivities)] = 0
+  connectivities = connectivities.astype(np.int32)
+  connectivities = nodes_map[connectivities]
+  labels          = np.array(elements.index)
+  etypes          = np.array([cell_map[t] for t in elements.etype])
+  lconn           = Ne + (connectivities != -1).sum()
+  if dataformat == "XML":
+    nodes_string = "\n".join([11*" " + "{0} {1} {2}".format(
+                              n.x, 
+                              n.y, 
+                              n.z) 
+                        for i, n in nodes.iterrows()])
+    elements_string = ""
+    for i in xrange(Ne):
+      elements_string += 11*" " + str(etypes[i]) + " "
+      c = connectivities[i]
+      c = c[np.where(c != -1)]
+      elements_string += " ".join([str(i) for i in c]) + "\n"
+    elements_string = element_string[:-1]  
+  
+  elif dataformat == "HDF":
+    hdf = pd.HDFStore(path + ".h5")
+    hdf.put("COORDS", mesh.nodes.data[list("xyz")])
+    flatconn = np.zeros(lconn, dtype = np.int32)
+    pos = 0
+    for i in xrange(Ne):
+      print i
+      c = connectivities[i]
+      c = c[np.where(c != -1)]
+      lc = len(c)
+      flatconn[pos] = etypes[i]
+      flatconn[pos + 1 + np.arange(lc)] = c
+      pos += 1 + lc
+    hdf.put("CONNECTIVITY", pd.DataFrame(flatconn))  
+    nodes_string = 11*" " + "{0}.h5:/COORDS/block0_values".format(path)
+    elements_string = 11*" " + "{0}.h5:/CONNECTIVITY/block0_values".format(path)
+    
+    hdf.close()
+  pattern = pattern.replace("#ELEMENT_NUMBER", str(Ne))
+  pattern = pattern.replace("#CONN_DIMENSION", str(lconn))
+  pattern = pattern.replace("#CONN_PATH", elements_string)
+  pattern = pattern.replace("#NODE_NUMBER", str(Nn))
+  pattern = pattern.replace("#NODE_PATH", nodes_string)
+  pattern = pattern.replace("#DATAFORMAT", dataformat)
+  open(path + ".xdmf", "wb").write(pattern)
+  
+     
+ 
+    
+  t2 =  time.time()
+  print "# => {0:.2f}s".format(t2-t1)  
--- doc/mesh/demo.geo
+++ doc/mesh/demo.geo
@@ -1,6 +1,6 @@
-lcar     = .05;
+lcar     = 1.;
 Point(1) = {0., 0., 0., lcar};
-Point(2) = {1., 0., 0., lcar*2.0};
+Point(2) = {1., 0., 0., lcar*10.0};
 Point(3) = {1., 1., 0., lcar};
 Point(4) = {0., 1., 0., lcar/10.};
 Line(1)  = {1,2};
@@ -10,6 +10,6 @@ Line(4)  = {4,1};
 Line Loop(1)        = {1,2,3,4};
 Plane Surface(1)    = {1};
 //Transfinite Surface {1};
-Recombine Surface {1};
+//Recombine Surface {1};
 Physical Surface("Front") = 1;
 
